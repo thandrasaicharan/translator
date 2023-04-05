60ff7ca15a43a277074b50fa73d649d2a15c9692
Charan <Charansthandra@gmail.com>
2023-04-06 04:58:17 +0530

    feature update

---
+++ translator-client/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.translator.app',
+  appName: 'translator-client',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
